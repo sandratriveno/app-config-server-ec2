1803ecf0e75452f39002ae38c34b4e4c995bddce
sandratriveno <sandra.triveno@gmail.com>
2025-02-23 10:29:02 -0500

    initial project

---
+++ src/main/java/pe/edu/idat/app_config_server_ec2/AppConfigServerEc2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_config_server_ec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigServerEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfigServerEc2Application.class, args);
+	}
+
+}
